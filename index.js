0965cc9340e9c10b8161a57982afe58fe5160ac7
Ben Beaudet <bbeaudet0@gmail.com>
2025-05-05 23:22:06 -0400

    Added Ace functionality, updated scoring and formatting

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+// INITIALIZATION
 let player = {
-    name: "Ben",
-    chips: 0,
+    name: "Player",
+    chips: 10,
     wins: 0,
     losses: 0
 }
-
 let cards = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let isPassed = false
+let permaDeath = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 let winLossEl = document.getElementById("winloss-el")
+let startButtonEl = document.getElementById("start-button-el")
+let newCardButtonEl = document.getElementById("newcard-button-el")
+let passButtonEl = document.getElementById("pass-button-el")
+updateButtonStates()
 updateStats()
 
-function updateStats() {
-    playerEl.textContent = player.name + ": $" + player.chips
-    winLossEl.textContent = "Wins: " + player.wins + "  /  Losses: " + player.losses
+function startGame() {
+    if (permaDeath === false && isAlive === false) {
+        isAlive = true
+        isPassed = false
+        hasBlackJack = false
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+        console.log("Game Started")
+        updateButtonStates()
+        renderGame()
+    }
 }
 
 function getRandomCard() {
@@ -34,18 +49,6 @@ function getRandomCard() {
     }
 }
 
-function startGame() {
-    isAlive = true
-    isPassed = false
-    hasBlackJack = false
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    console.log("Game Started")
-    renderGame()
-}
-
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
@@ -56,6 +59,7 @@ function renderGame() {
         if (isPassed === false) {
             message = "Do you want to draw a new card?"
         } else {
+            // could possibly move this to its own function createDealerHand()
             let dealerHand = 0
             let randomHand = Math.floor(Math.random()*50)+1
             // nat21=4.8, 21=7.4, 20=17.6, 19=13.5, 18=13.8, 17=14.6, 16=28.4, 15=0
@@ -67,17 +71,18 @@ function renderGame() {
             else if (randomHand <= 47) {dealerHand = 16}
             else if (randomHand <= 49) {dealerHand = 15}
             else if (randomHand <= 50) {dealerHand = 14}
-            console.log(randomHand," ",dealerHand)
+            console.log("Random Number from 1 to 50: ",randomHand,", Dealer hand is ",dealerHand)
             if (sum < dealerHand) {
                 message = "Dealer wins with " + dealerHand
                 isAlive = false
                 console.log("Dealer Wins")
-                updateStats(player.chips += -1, player.losses += 1)
+                updateStats(player.chips = Math.floor(player.chips/2), player.losses += 1)
             } else if (sum > dealerHand) {
                 message = "Dealer has " + dealerHand + ", you win!"
                 hasBlackJack = true // could amend this by adding new beatDealer boolean
+                isAlive = false
                 console.log("Beat Dealer")
-                updateStats(player.chips += 5, player.wins += 1)
+                updateStats(player.chips = Math.ceil(player.chips*2), player.wins += 1)
             } else {
                 message = "Dealer has " + dealerHand + ", it's a tie!"
                 isAlive = false
@@ -88,15 +93,25 @@ function renderGame() {
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        isAlive = false
         console.log("Blackjack")
-        updateStats(player.chips += 5, player.wins += 1)
+        updateStats(player.chips = Math.ceil(player.chips*2), player.wins += 1)
     } else {
+        for (let i = 0; i < cards.length; i++) { // turns Ace into a 1 if it would cause you to Bust
+            if (cards[i] === 11) {
+                cards[i] += -10
+                sum += -10
+                renderGame()
+                return
+            }        
+        }
         message = "You're out of the game!"
         isAlive = false
         console.log("Bust")
-        updateStats(player.chips += -1, player.losses += 1)
+        updateStats(player.chips = Math.floor(player.chips/2), player.losses += 1)
     }
     messageEl.textContent = message
+    updateButtonStates()
     console.log("Game Rendered")
 }
 
@@ -106,6 +121,7 @@ function newCard() {
         sum += card
         cards.push(card)
         console.log("New Card")
+        updateButtonStates()
         renderGame()        
     }
 }
@@ -114,6 +130,34 @@ function passCard() {
     if (isAlive === true && hasBlackJack === false) {
         isPassed = true
         console.log("Pass")
+        updateButtonStates()
         renderGame()        
     }
+}
+
+function updateButtonStates() {
+    if (isAlive === true) {
+        startButtonEl.style.opacity = 0.5
+        newCardButtonEl.style.opacity = 1
+        passButtonEl.style.opacity = 1
+    } else if (permaDeath === true) {
+        startButtonEl.style.opacity = 0.5
+        newCardButtonEl.style.opacity = 0.5
+        passButtonEl.style.opacity = 0.5
+    } else {
+        startButtonEl.style.opacity = 1
+        newCardButtonEl.style.opacity = 0.5
+        passButtonEl.style.opacity = 0.5
+    }
+}
+
+function updateStats() {
+    if (player.chips <= 0) {
+        permaDeath = true
+        message = "You're broke! Try again next time..."
+        messageEl.style.color="red"
+        playerEl.style.color="red"
+    }
+    playerEl.textContent = player.name + ": $" + player.chips
+    winLossEl.textContent = "Wins: " + player.wins + "  /  Losses: " + player.losses
 }
